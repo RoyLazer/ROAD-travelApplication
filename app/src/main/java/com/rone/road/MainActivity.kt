e5068668f41d2ba9d4f378fb427626382850d1ba
RoyLazer <chido-dante@hotmail.com>
2020-12-18 23:49:24 -0600

    After all I've eliminated all the icons that I've added before, this time I've created and positioned my buttons for the login activity, also I've created my own navigation drawer.

--- app/src/main/java/com/rone/road/MainActivity.kt
+++ app/src/main/java/com/rone/road/MainActivity.kt
@@ -2,10 +2,50 @@ package com.rone.road
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+import com.rone.road.R.id.drawerLayout
+import com.rone.road.R.id.navView
+import com.rone.road.R.layout.activity_main
 
 class MainActivity : AppCompatActivity() {
+    //lateinit makes the promise that we will initialize the toggle
+    lateinit var toggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(activity_main)
+
+        //Let's create the toggle button
+        toggle = ActionBarDrawerToggle(this, findViewById(drawerLayout) , R.string.open, R.string.close)
+        //remember to specify the kind of object that we're looking for 'drawerlayout'
+        findViewById<DrawerLayout>(drawerLayout).addDrawerListener(toggle)
+        toggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        findViewById<NavigationView>(navView).setNavigationItemSelectedListener{
+            when(it.itemId){
+                R.id.myItem1 -> Toast.makeText(applicationContext, "Presionaste el boton 1", Toast.LENGTH_LONG).show()
+                R.id.myItem2 -> Toast.makeText(applicationContext, "Presionaste el boton 2", Toast.LENGTH_LONG).show()
+                R.id.myItem3 -> Toast.makeText(applicationContext, "Presionaste el boton 3", Toast.LENGTH_LONG).show()
+            }
+            true
+        }
+
     }
+    //this function will make the items select
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
+
+
 }
--- app/src/main/java/com/rone/road/loginActivity.kt
+++ app/src/main/java/com/rone/road/loginActivity.kt
@@ -1,11 +1,17 @@
 package com.rone.road
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class loginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
+    fun newLogin(view: View){
+        val startMain = Intent(this,MainActivity::class.java)
+        startActivity(startMain)
     }
 }
